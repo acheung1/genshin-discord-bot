2a81054081c2704b512553607ec12763e1621978
andrew <andrewcheungmd@gmail.com>
2021-03-09 19:24:46 -0500

    added hide/unhide option to wholesome command :)

--- commands/wholesome.js
+++ commands/wholesome.js
@@ -3,10 +3,16 @@ module.exports = {
     description: 'wholesome berto experience',
     premissions: [],
     async execute(client, message, args, Discord) {
-        message.channel.send(`\`mfw\njust had the most wholesome coop experience in my life\nafter getting my ass whooped in numerous coop worlds\ni happened into this one group\``)
-        .then(() => message.delete())
-        .catch((err) => {
-            console.log(err);
-        });
+        if (args && args.length <= 1) {
+            message.channel.send(`\`mfw\njust had the most wholesome coop experience in my life\nafter getting my ass whooped in numerous coop worlds\ni happened into this one group\``)
+            .then(() => {
+                if (args.length === 1 && args[0] === 'h') {
+                    message.delete();
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }
     }
 }
